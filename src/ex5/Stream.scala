6a2fae3f321719b68054311eb636e3ef77f7963a
batangr00t <batangr00t@daum.net>
2015-10-13 21:38:25 +0900

    ex5

--- src/ex5/Stream.scala
+++ src/ex5/Stream.scala
@@ -1,19 +1,20 @@
 package ex5
 
 sealed trait Stream[+A] {
+  
   def headOption: Option[A] = this match {
     case Empty      => None
-    case Cons(h, t) => Some(h())
+    case Cons(h, t) => Some(h)
   }
 
   def toList(): List[A] = this match {
     case Empty      => List()
-    case Cons(h, t) => List(h()) ::: t().toList()
+    case Cons(h, t) => List(h) ::: t().toList()
   }
 
   def take(n: Int): Stream[A] = this match {
-    case Cons(h, t) if (n > 0) => Cons(h, () => t().take(n - 1))
-    case _                     => Empty
+    case Cons(h, t) if (n > 0) => Stream.cons(h, t().take(n-1))
+    case _                     => Stream.empty
   }
 
   def drop(n: Int): Stream[A] = this match {
@@ -22,24 +23,53 @@ sealed trait Stream[+A] {
   }
 
   def takeWhile(p: A => Boolean): Stream[A] = this match {
-    case Empty      => Empty
-    case Cons(h, t) => if (p(h())) Cons(h, () => t().takeWhile(p)) else t().takeWhile(p)
+    case Empty      => Stream.empty
+    case Cons(h, t) => if (p(h)) Stream.cons( h, t().takeWhile(p)) else t().takeWhile(p)
   }
+
+  def exists(p: A => Boolean): Boolean = this match {
+    case Empty      => false
+    case Cons(h, t) => p(h) || t().exists(p)
+  }
+
+  def forall(p: A => Boolean): Boolean = this match {
+    case Empty      => true
+    case Cons(h, t) => p(h) && t().forall(p)
+  }
+
+  def foldRight[B](z: => B)(f: (A, => B) => B): B = {
+    this match {
+      case Cons(h, t) => f(h, t().foldRight(z)(f))
+      case _          => z
+    }
+  }
+
+  class ConsWrapper[A](tl: => Stream[A]) {
+    println("ConsWrapper is created")
+    def #::(hd: A): Stream[A] = Stream.cons(hd, tl) 
+  }
+  
+  implicit def consWrapper[A](stream: () => Stream[A]): ConsWrapper[A] =
+    new ConsWrapper[A](stream())
+
 }
 
 case object Empty extends Stream[Nothing] {
   println("Empty is created")
 }
-case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A] {
-  println("Cons is created")
+case class Cons[+A](h: A, tl: () => Stream[A]) extends Stream[A] {
+  println("class Cons is created")
 }
-
+  
 object Stream {
-  def cons[A](hd: => A, tl: => Stream[A]): Stream[A] = {
-    lazy val head = hd
-    lazy val tail = tl
 
-    Cons(() => head, () => tail)
+  object cons {
+    def apply[A](head: A, tail: => Stream[A]): Stream[A] = {
+      println("cons() is called")
+      lazy val t = tail
+  
+      new Cons( head, () => t)
+    }
   }
 
   def empty[A]: Stream[A] = Empty
--- src/ex5test.scala
+++ src/ex5test.scala
@@ -1,5 +1,5 @@
 import ex5._
-import cho.util
+//import cho.util
 
 /**
  * @author batangr00t@daum.net
@@ -8,22 +8,36 @@ object ex5test extends App {
   
   // ex5.1
   println("ex5.1----------------------------------")
-  val l = Stream(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
-  println( l.toList() )
+  //val l = 0 #:: Stream(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+  println( l ) 
 
   println("ex5.2----------------------------------")
   val l2 = l.take(7)
-  println( l2 )
-  println( l2.toList() )
-  println( l.take(11).toList() )
-  
-  println( l.drop(1) )
-  println( l.drop(1).toList() )
-  println( l.drop(7).toList() )
-  println( l.drop(11).toList() )
+
+  println( l2.toList )
+  //println( l.take(11).toList )
+  //println( l.drop(1) )
+  //println( l.drop(1).toList )
+  println( l.drop(7).toList )
+  //println( l.drop(11).toList )
   
   println("ex5.3----------------------------------")
   val l3 = l.takeWhile(_ < 7)
   println( l3 )
-  //println( l3.toList() )
+  //println( l3.toList )
+  
+  println("ex5.4----------------------------------")
+  
+  val l4: Stream[Int] = {
+    def loop(v: => Int): Stream[Int] = if ( v < 100 ) v #:: loop(v+1) else Stream.empty
+    loop(1)
+  } 
+  println( l4.forall(_ < 7) )
+
+//  def pairIf[A](b:Boolean, x: => A) = {
+//    //lazy val y = x
+//    if (b) (x,x)
+//  }
+//  
+//  pairIf( false, { println("xx"); 3 } ) 
 }
