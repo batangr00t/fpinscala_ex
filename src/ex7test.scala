59a5e57caa4670020da9c09c26d14d772e52032e
batangr00t <batangr00t@daum.net>
2015-10-21 22:59:56 +0900

    ex7 예제

--- src/ex7test.scala
+++ src/ex7test.scala
@@ -1,5 +1,9 @@
 import ex7._
 
-object ex7test {
+object ex7test extends App {
+  val co = new Callable[A] {    def call = a(es).get }
+  val par = Par.unit(1)
+  println( par  )
   
+  println("ex7.1----------------------------------")
 }
